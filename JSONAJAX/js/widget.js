953d212e1b6f12854043e8121ef70ca57e20b7d7
Dark-Slide <martin.haagendrup@hotmail.com>
2021-07-07 14:48:06 +0200

    Added rooms.json and updated widget.js

--- JSONAJAX/js/widget.js
+++ JSONAJAX/js/widget.js
@@ -1,7 +1,7 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-	if(xhr.readyState === 4) {
-		var employees = JSON.parse(xhr.responseText);
+var isHere = new XMLHttpRequest();
+isHere.onreadystatechange = function() {
+	if(isHere.readyState === 4) {
+		var employees = JSON.parse(isHere.responseText);
 		var statusHTML = '<ul class="bulleted">';
 		for (var i = 0; i < employees.length; i++) {
 			if (employees[i].inoffice === true) {
@@ -16,5 +16,26 @@ xhr.onreadystatechange = function() {
 		document.getElementById('employeeList').innerHTML = statusHTML;
 	}
 };
-xhr.open('GET', 'data/employees.json');
-xhr.send();
+isHere.open('GET', 'data/employees.json');
+isHere.send();
+
+var roomRequest = new XMLHttpRequest();
+roomRequest.onreadystatechange = function() {
+	if(roomRequest.readyState === 4) {
+		var rooms = JSON.parse(roomRequest.responseText);
+		var statusHTML = '<ul class="rooms">';
+		for (var i = 0; i < rooms.length; i++) {
+			if (rooms[i].inoffice === true) {
+				statusHTML += '<li class="empty">';
+			} else {
+				statusHTML += '<li class="full">';
+			}
+			statusHTML += rooms[i].room;
+			statusHTML += '</li>';
+		}
+		statusHTML += '</ul>';
+		document.getElementById('roomList').innerHTML = statusHTML;
+	}
+};
+roomRequest.open('GET', 'data/rooms.json');
+roomRequest.send();
