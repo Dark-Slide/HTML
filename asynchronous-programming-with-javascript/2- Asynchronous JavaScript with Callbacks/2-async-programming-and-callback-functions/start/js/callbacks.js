db216429021f569faa101ed024d74c9ec468166c
Dark-Slide <martin.haagendrup@hotmail.com>
2021-07-08 09:03:43 +0200

    Update callbacks.js

--- asynchronous-programming-with-javascript/2- Asynchronous JavaScript with Callbacks/2-async-programming-and-callback-functions/start/js/callbacks.js
+++ asynchronous-programming-with-javascript/2- Asynchronous JavaScript with Callbacks/2-async-programming-and-callback-functions/start/js/callbacks.js
@@ -1,4 +1,4 @@
-const astrosUrl = 'asteros.json';
+const astrosUrl = 'astros.json';
 const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
